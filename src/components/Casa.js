0847db4d24ee1c2f8124bfa08395fb3019cd9703
Gustavo <gustavojaviernunezdiaz@gmail.com>
2026-04-22 19:10:00 -0300

    Refactor Casa component for improved readability; streamline JSX structure and maintain consistent formatting

--- src/components/Casa.js
+++ src/components/Casa.js
@@ -12,30 +12,27 @@ import Sala from "./Sala";
 import SalaJuegos from "./SalaJuegos";
 
 const Casa = () => {
-  return (
-    <SafeAreaView style={[styles.container, { backgroundColor: '#111827' }]}>
-     
-      <View style={styles.panelControl}>
-        <Text style={styles.texto}>PANEL</Text>
-        <PanelControl />
-      </View>
+    return (
+        <SafeAreaView style={[styles.container, { backgroundColor: '#111827' }]}>
+            <View style={styles.panelControl}>
+                <Text style={styles.texto}>PANEL</Text>
+                <PanelControl/> 
+            </View>
 
- 
-      <View style={styles.habitacionesContainer}>
-        <Cocina />
-        <Sala />
-        <Baño />
-        <Dormitorio />
-        <Dormitorio2 />
-        <SalaJuegos />
-      </View>
+            <View style={styles.habitacionesContainer}>
+                <Cocina />
+                <Baño/>
+                <Dormitorio/>   
+                <Dormitorio2/>  
+                <Sala/> 
+                <SalaJuegos/>   
+            </View>
 
-      
-      <View style={styles.consumo}>
-        <Consumo />
-      </View>
-    </SafeAreaView>
-  );
-};
+            <View style={styles.consumo}>
+                <Consumo />
+            </View>
+        </SafeAreaView>
+    );
+}
 
-export default Casa;
+export default Casa; 
